542fbb9d2733fcc5a2970fac9b2c506d591986c7
eyosen <abittin@gmail.com>
2015-12-09 11:27:15 +0000

    Add Screen Record to the Power Menu [1/2]
    - Adapted to DU's Power Menu
    
    Change-Id: I841b7e7521660676c7eba47388ae233237678321

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3966,6 +3966,13 @@ public final class Settings {
         public static final String POWERMENU_POWER = "powermenu_power";
 
         /**
+         * Whether to display screen record in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_SCREENRECORD = "powermenu_screenrecord";
+
+        /**
          * Volume rocker wake
          * @hide
          */
